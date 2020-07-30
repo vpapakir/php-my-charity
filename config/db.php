793c99699c68f163d80be7d962c47634ab33eee3
Evangelos Papakirykou <vpapakir@gmail.com>
2020-07-30 00:10:48 +0000

    moved out config

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=flynaut_charity',
-    'username' => 'flynaut_charity',
-    'password' => '7kTwaB0U&4iC',
-    'charset' => 'utf8',
-];
